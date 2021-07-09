d4eaeefa2915b6c4ece3e39e3c2939e88bc3f599
Franco Borgiani <francoborgiani@outlook.com>
2021-07-09 12:47:50 -0300

    Version final

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ function Move(value) {
     const trackWidth = track.offsetWidth;
     const listWidth = slickList.offsetWidth;
 
-    track.style.left == "" ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0, -2) * -1);
+    track.style.left == "" ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0, -2.7) * -1);
 
     if (leftPosition < (trackWidth - listWidth) && value == 2) {
         track.style.left = `${-1 * (leftPosition + slickWidth)
@@ -22,3 +22,52 @@ function Move(value) {
         track.style.left =`${-1 * (leftPosition - slickWidth)}px`;
     }
 }
+if (window.innerWidth <= 720) {
+    function Move(value) {
+        const trackWidth = track.offsetWidth;
+        const listWidth = slickList.offsetWidth;
+    
+        track.style.left == "" ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0, -2.7) * -1);
+    
+        if (leftPosition < (trackWidth - listWidth) && value == 2) {
+            track.style.left = `${-1 * (leftPosition + slickWidth)
+        }px`;
+        }else if(leftPosition > 0 && value == 1){
+            track.style.left =`${-1 * (leftPosition - slickWidth)}px`;
+        }
+    }
+}else {
+    function Move(value) {
+        const trackWidth = track.offsetWidth;
+        const listWidth = slickList.offsetWidth;
+    
+        track.style.left == "" ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0, -2) * -1);
+    
+        if (leftPosition < (trackWidth - listWidth) && value == 2 && track.style.left >= -1800 ) {
+            track.style.left = `${-1 * (leftPosition + slickWidth)
+        }px`;
+        }else if(leftPosition > 0 && value == 1){
+            track.style.left =`${-1 * (leftPosition - slickWidth)}px`;
+        }
+    }
+}
+
+// Menu
+
+window.addEventListener('DOMContentLoaded', () => {
+    const buttonMenu = document.querySelector('.btn-menu');
+    const menuList = document.querySelector('.header__nav--list');
+    buttonMenu.addEventListener('click', () => {
+        menuList.classList.toggle('show');
+    })
+    menuList.addEventListener('click', () => {
+        menuList.classList.toggle('show');
+    })
+});
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('.header__nav');
+    header.classList.add('header__scroll');
+    if (window.scrollY == 0) {
+        header.classList.remove('header__scroll')
+    }
+})
